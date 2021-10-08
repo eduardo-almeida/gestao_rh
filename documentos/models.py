cb11c280558ef9258e1e0d8069cf4cbd419bdb24
unknown <eduardoeandroid@gmail.com>
2021-10-08 09:50:32 -0300

    Organizando app

--- documentos/models.py
+++ documentos/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from colaboradores.models import Colaborador
 
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=100)
+    colaborador = models.ForeignKey(Colaborador, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.descricao
