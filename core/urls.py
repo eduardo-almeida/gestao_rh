fb2705e3d1a1fc5624f3f210b7bb767d07e0d492
unknown <eduardoeandroid@gmail.com>
2021-10-08 13:44:12 -0300

    Form de nova empresa

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('inicio.urls')),
     path('colaboradores/', include('colaboradores.urls')),
+    path('empresa/', include('empresas.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
