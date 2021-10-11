05bcd8d67a11bde6fb39698c410a4e024ab7b4d5
unknown <eduardoeandroid@gmail.com>
2021-10-11 08:06:47 -0300

    Crud colaborador Completo

--- colaboradores/urls.py
+++ colaboradores/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ColaboradoresList, ColaboradoresEdit, ColaboradoresDelete
+from .views import ColaboradoresList, ColaboradoresEdit, ColaboradoresDelete, ColaboradoresCreate
 
 urlpatterns = [
     path('', ColaboradoresList.as_view(), name='list_colaboradores'),
     path('editar/<int:pk>/', ColaboradoresEdit.as_view(), name='update_colaboradores'),
     path('deletar/<int:pk>/', ColaboradoresDelete.as_view(), name='delete_colaboradores'),
+    path('novo/', ColaboradoresCreate.as_view(), name='create_colaboradores'),
 ]
--- colaboradores/views.py
+++ colaboradores/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+
 from .models import Colaborador
-from django.views.generic import ListView, UpdateView, DeleteView
+from django.views.generic import ListView, UpdateView, DeleteView, CreateView
 
 
 class ColaboradoresList(ListView):
@@ -19,5 +22,17 @@ class ColaboradoresEdit(UpdateView):
 
 class ColaboradoresDelete(DeleteView):
     model = Colaborador
-    success_url = 'list_colaboradores'
+    success_url = reverse_lazy('list_colaboradores')
+
+class ColaboradoresCreate(CreateView):
+    model = Colaborador
+    fields = ['nome', 'departamentos']
+
+    def form_valid(self, form):
+        colaborador = form.save(commit=False)
+        colaborador.empresa = self.request.user.colaborador.empresa
+        username = colaborador.nome.split(' ')[0] + colaborador.nome.split(' ')[1]
+        colaborador.user = User.objects.create(username=username)
+        colaborador.save()
+        return super(ColaboradoresCreate, self).form_valid(form)
 
