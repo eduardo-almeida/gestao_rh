ba8653da6a791092ebce9a8d032d66ab8498410e
unknown <eduardoeandroid@gmail.com>
2021-10-13 14:19:29 -0300

    Diferentes redirecionamento

--- registro_hora_extra/urls.py
+++ registro_hora_extra/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import HoraExtraList, HoraExtraEdit, HoraExtraDelete, HoraExtraCreate
+from .views import (
+    HoraExtraList,
+    HoraExtraEdit,
+    HoraExtraEditBase,
+    HoraExtraDelete,
+    HoraExtraCreate)
 
 urlpatterns = [
     path('', HoraExtraList.as_view(), name='list_hora_extra'),
-    path('editar/<int:pk>/', HoraExtraEdit.as_view(), name='update_hora_extra'),
+    path('editar-colaborador/<int:pk>/', HoraExtraEdit.as_view(), name='update_hora_extra'),
+    path('editar/<int:pk>/', HoraExtraEditBase.as_view(), name='update_hora_extra_base'),
     path('deletar/<int:pk>/', HoraExtraDelete.as_view(), name='delete_hora_extra'),
     path('novo/', HoraExtraCreate.as_view(), name='create_hora_extra'),
 ]
--- registro_hora_extra/views.py
+++ registro_hora_extra/views.py
@@ -23,6 +23,19 @@ class HoraExtraEdit(UpdateView):
         kwargs.update({'user': self.request.user})
         return kwargs
 
+class HoraExtraEditBase(UpdateView):
+    model = RegistroHoraExtra
+    form_class = RegistroHoraExtraForm
+    #success_url = reverse_lazy('list_hora_extra')
+
+    def get_success_url(self):
+        return reverse_lazy('update_hora_extra_base', args=[self.object.id])
+
+    def get_form_kwargs(self):
+        kwargs = super(HoraExtraEditBase, self).get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return kwargs
+
 
 class HoraExtraDelete(DeleteView):
     model = RegistroHoraExtra
