fdcf00e7eb8f3ae8179b3aad801e8cd7c8b07f8f
unknown <eduardoeandroid@gmail.com>
2021-10-15 09:18:09 -0300

    Criacao de banco de dados antigo

--- core/settings.py
+++ core/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'documentos',
     'registro_hora_extra',
     'inicio',
+    'app_antiga',
     'rest_framework',
     'rest_framework.authtoken',
 ]
@@ -63,6 +64,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'antigo': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'dbantigo.sqlite3',
     }
 }
 
@@ -108,3 +113,5 @@ STATICFILES_DIRS = [
 LOGIN_REDIRECT_URL = 'home'
 
 LOGOUT_REDIRECT_URL = 'login'
+
+DATABASES_ROUTERS = ['gestao_rh.DBRoutes.DBRoutes']
